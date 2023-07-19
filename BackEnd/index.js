309fd83e558a6bb8068a65ed21c6b7a4710d18b4
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-19 19:45:31 +0530

    some changes in backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 app.use(cors())
 const port = process.env.port || 8000;
 connectToMongoose();
-app.use(express.json());
+app.use(express.    json());
 app.use('/api/user',require('./routes/user'));
 app.use('/api/admin',require('./routes/admin'));
 app.use('/api/books',require('./routes/book'));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} and http://localhost:${port}`)
 })
+
