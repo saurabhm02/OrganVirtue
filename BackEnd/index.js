f02ca5ae3ccac10fafc03a191dbda71a68097af3
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-19 19:20:47 +0530

    some changes in backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors())
-const port = 8000;
+const port = process.env.port || 8000;
 connectToMongoose();
 app.use(express.json());
 app.use('/api/user',require('./routes/user'));
